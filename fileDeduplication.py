1d5a782a8905639d4bf0d44a98e5ccad1e3bbae6
3gstudent <15814408+3gstudent@users.noreply.github.com>
2019-05-10 05:57:01 -0700

    Create fileDeduplication.py

---
+++ fileDeduplication.py
@@ -0,0 +1,20 @@
+import shutil
+def filededuplication(path)
+  lines_seen = set()
+  outfile=open(path+"new","w")
+  f = open(path,"r")
+  for line in f:
+    if line not in lines_seen:
+      outfile.write(line)
+      lines_seen.add(line)
+  outfile.close()
+  print("[*]done")
+
+if __name__ == '__main__':
+  if len(sys.argv)!=2:
+    print('[!]Wrong parameter')
+    print('Usage:')
+    print('%s <filepath>'%(sys.argv[0]))
+    sys.exit(0)
+  else:
+    filededuplication(sys.argv[1])
