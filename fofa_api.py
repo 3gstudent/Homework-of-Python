1870415faf61b1f56039da74756873082c51e211
3gstudent <15814408+3gstudent@users.noreply.github.com>
2019-09-25 00:59:33 -0700

    Create fofa_api.py

---
+++ fofa_api.py
@@ -0,0 +1,23 @@
+import urllib2
+import sys 
+import base64 
+import os,json 
+email="xx@xx.com"
+key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+def search(keywords): 
+    vulhostlist=[] 
+    urlenkeywords=urllib2.quote(keywords)    
+    searchurl="https://fofa.so/api/v1/search/all?email="+email+"&key="+key+"&qbase64="+base64.b64encode(keywords)
+
+    req=urllib2.urlopen(searchurl)
+    results=req.read()
+    data=json.loads(results)
+    for ip in data["results"]:
+        print ip[0]
+     
+if __name__=="__main__": 
+    if len(sys.argv)!=2:
+        print ('[!]Wrong parameter') 
+        sys.exit() 
+    else: 
+        search(sys.argv[1])
