7e40a415db20d230908453d477a2f8cfd0887e32
3gstudent <15814408+3gstudent@users.noreply.github.com>
2021-09-19 16:31:54 -1000

    Update fofa_api_VIP.py

--- fofa_api_VIP.py
+++ fofa_api_VIP.py
@@ -1,23 +1,27 @@
-import urllib2
+#python3
+import requests
+from urllib.parse import urlparse, urljoin, quote
+import urllib3
+urllib3.disable_warnings()
 import sys 
 import base64 
 import os,json 
 email="xx@xx.com"
 key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 def search(keywords): 
-    vulhostlist=[] 
-    urlenkeywords=urllib2.quote(keywords)    
-    searchurl="https://fofa.so/api/v1/search/all?email="+email+"&key="+key+"&size=10000"+"&qbase64="+base64.b64encode(keywords)
-
-    req=urllib2.urlopen(searchurl)
-    results=req.read()
-    data=json.loads(results)
+    vulhostlist = [] 
+    urlenkeywords = quote(keywords)    
+    searchurl = "https://fofa.so/api/v1/search/all?email="+email+"&key="+key+"&size=10000"+"&qbase64="+base64.b64encode(keywords.encode("utf-8")).decode('utf8')
+    print(searchurl)
+    req = requests.get(searchurl, verify=False)
+    data = req.json()
     for ip in data["results"]:
-        print ip[0]
+        print(ip[0])
      
-if __name__=="__main__": 
+if __name__ == "__main__": 
     if len(sys.argv)!=2:
         print ('[!]Wrong parameter') 
         sys.exit() 
     else: 
         search(sys.argv[1])
+        
